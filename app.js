d999d82fa4c1d07372ef79795dcf65a043e99d0d
KanchanSharmaN <sharmakanchan0209@gmail.com>
2025-08-07 18:03:56 +0530

    Ensure views and public folders are included

--- app.js
+++ app.js
@@ -199,4 +199,7 @@ app.get("/show/:id", async (req,res)=>{
     }
 })
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
